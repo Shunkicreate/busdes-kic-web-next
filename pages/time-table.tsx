cd6b082c80192d71f4a52ae3e67e52134581e3d5
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-05-14 07:37:56 +0900

    eslint系

--- pages/time-table.tsx
+++ pages/time-table.tsx
@@ -1,7 +1,10 @@
 const TimeTable = () => {
-    return (
-        <div>TimeTable</div>
-    )
+	const a = 'fa'
+	const b = 'fa'
+	const c = 'Afa'
+	const d = 'ahfda'
+	const e = 'hogehoge'
+	return <div>TimeTable</div>
 }
 
 export default TimeTable
