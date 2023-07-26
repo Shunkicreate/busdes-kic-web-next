55eb9c1ccff508ee2eb4d07d3e2fc5f51e5bc08e
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-07-27 04:08:04 +0900

    re add pwd

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    compiler: {
+        // removeConsole: process.env.NODE_ENV !== "development",
+    },
+};
 
-module.exports = nextConfig
+const withPWA = require("next-pwa")({
+    dest: "public",
+    // disable: process.env.NODE_ENV === "development",
+    register: true,
+    skipWaiting: true,
+});
+
+module.exports = withPWA(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,52 +1,49 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: 'Busdes!【衣笠】',
-	description: 'Busdes!【衣笠】衣笠生向けバスアプリ',
-	themeColor: '#F8F8F8',
-	manifest: '/manifest.json',
+	title: "Busdes!【衣笠】",
+	description: "Busdes!【衣笠】衣笠生向けバスアプリ",
+	themeColor: "#F8F8F8",
+	manifest: "/manifest.json",
 	icons: [
-		{ rel: 'icon', type: 'image/png', sizes: '16x16', url: '/favicon-16x16.png' },
-		{ rel: 'icon', type: 'image/png', sizes: '32x32', url: '/favicon-32x32.png' },
-		{ rel: 'apple-touch-icon', type: 'image/png', sizes: '180x180', url: '/apple-touch-icon.png' },
+		{ rel: "icon", type: "image/png", sizes: "16x16", url: "/favicon-16x16.png" },
+		{ rel: "icon", type: "image/png", sizes: "32x32", url: "/favicon-32x32.png" },
+		{ rel: "apple-touch-icon", type: "image/png", sizes: "180x180", url: "/apple-touch-icon.png" },
 	],
 	appleWebApp: {
 		capable: true,
-		title: 'Busdes!【衣笠】',
-		statusBarStyle: 'black-translucent',
+		title: "Busdes!【衣笠】",
+		statusBarStyle: "black-translucent",
 	},
 	openGraph: {
-		type: 'website',
-		url: '%PUBLIC_URL%',
-		title: 'Busdes!【衣笠】',
-		description: 'Busdes!【衣笠】衣笠生向けバスアプリ',
-		siteName: 'Busdes!【衣笠】',
+		type: "website",
+		url: "%PUBLIC_URL%",
+		title: "Busdes!【衣笠】",
+		description: "Busdes!【衣笠】衣笠生向けバスアプリ",
+		siteName: "Busdes!【衣笠】",
 		images: [
 			{
-				url: '%PUBLIC_URL%/ogp-large.png',
+				url: "%PUBLIC_URL%/ogp-large.png",
 			},
 		],
 	},
 	twitter: {
-		card: 'summary_large_image',
-		title: 'Busdes!【衣笠】',
-		images: '%REACT_APP_TWITTER_CARD_URL%',
-		description: 'Busdes!【衣笠】衣笠生向けバスアプリ',
+		card: "summary_large_image",
+		title: "Busdes!【衣笠】",
+		images: "%REACT_APP_TWITTER_CARD_URL%",
+		description: "Busdes!【衣笠】衣笠生向けバスアプリ",
 	},
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang='en'>
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
-}
