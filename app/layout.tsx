a94bc1ee09ac47d93fc1d789f59c8caadf8326ae
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-07-26 12:35:08 +0900

    set meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,38 @@
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-	title: 'Busdes!',
+	title: 'Busdes!【衣笠】',
 	description: 'Busdes!【衣笠】衣笠生向けバスアプリ',
-
+	themeColor: '#F8F8F8',
+	manifest: '%PUBLIC_URL%/manifest.json',
+	icons: [
+		{ rel: 'icon', type: 'image/png', sizes: '16x16', url: '/favicon-16x16.png' },
+		{ rel: 'icon', type: 'image/png', sizes: '32x32', url: '/favicon-32x32.png' },
+		{ rel: 'apple-touch-icon', type: 'image/png', sizes: '180x180', url: '/apple-touch-icon.png' },
+	],
+	appleWebApp: {
+		capable: true,
+		title: 'Busdes!【衣笠】',
+		statusBarStyle: 'black-translucent',
+	},
+	openGraph: {
+		type: 'website',
+		url: '%PUBLIC_URL%',
+		title: 'Busdes!【衣笠】',
+		description: 'Busdes!【衣笠】衣笠生向けバスアプリ',
+		siteName: 'Busdes!【衣笠】',
+		images: [
+			{
+				url: '%PUBLIC_URL%/ogp-large.png',
+			},
+		],
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: 'Busdes!【衣笠】',
+		images: '%REACT_APP_TWITTER_CARD_URL%',
+		description: 'Busdes!【衣笠】衣笠生向けバスアプリ',
+	},
 }
 
 export default function RootLayout({
---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+const Home = () => {
+	return (
+		<>
+			<main className={styles.main}>
+				<Link href={`/next-bus`}>next-bus</Link>
+				<Link href={`/time-table`}>time-table</Link>
+			</main>
+
+			<footer className={styles.footer}>&copy; 2023- Shunki Tada All rights reserved.</footer>
+		</>
+	)
+}
+
+export default Home
+
