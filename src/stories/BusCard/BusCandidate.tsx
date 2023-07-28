cb69109ccd10bb95e1be9b0c545353a4e9f9dc93
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-07-28 14:45:37 +0900

    fix bus candidate

--- src/stories/BusCard/BusCandidate.tsx
+++ src/stories/BusCard/BusCandidate.tsx
@@ -1,21 +1,19 @@
 import BusCandidateProps from "@/entities/storybook/BusCandidate.type";
+import DisplayTime from "../DisplayTime/DisplayTime";
 
 const BusCandidate = (props: BusCandidateProps) => {
 	const { StartTime, EndTime, BusName, selected } = props;
 	return (
 		<div className={`justify-start items-start gap-6 inline-flex ${selected ? "text-AlertMain" : ""}`}>
 			<div className={`justify-start items-start gap-1 flex`}>
-				<div className='text-center text-base font-normal leading-snug'>
-					{StartTime.getHours()}:{StartTime.getMinutes()}
-				</div>
+				<DisplayTime hour={StartTime.getHours()} minute={StartTime.getMinutes()} fontSize='text-base' />
 				<div className='text-center text-lg font-normal leading-snug'>→</div>
-				<div className='text-center text-base font-normal leading-snug'>
-					{EndTime.getHours()}:{EndTime.getMinutes()}
-				</div>
+				<DisplayTime hour={StartTime.getHours()} minute={StartTime.getMinutes()} fontSize='text-base' />
 			</div>
-			<div className='text-center text-sm font-normal leading-snug'>{BusName}</div>
+			<div className='text-center text-sm m-auto'>{BusName}</div>
 		</div>
 	);
 };
 
 export default BusCandidate;
+
