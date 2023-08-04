c420eab221ceea7857d902e7b158618d0827adc2
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-08-04 16:19:42 +0900

    add Interaction tests

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,23 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-    '@storybook/addon-a11y',
-  ],
-  framework: {
-		name: '@storybook/nextjs',
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-onboarding",
+		"@storybook/addon-interactions",
+		"@storybook/addon-a11y",
+		"@storybook/addon-coverage",
+	],
+	framework: {
+		name: "@storybook/nextjs",
 		options: {},
 	},
-	staticDirs: ['../public'],
-  docs: {
-    autodocs: "tag",
-  },
+	staticDirs: ["../public"],
+	docs: {
+		autodocs: "tag",
+	},
 };
 export default config;
+
